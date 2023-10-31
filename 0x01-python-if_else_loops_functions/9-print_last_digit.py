89e074199ebe74bd190cbc00d44c05f591ca8c11
houssam980 <houssam.ghmari1243@gmail.com>
2023-10-31 16:12:27 -0700

    last dijit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
- print("{:d}".format( - (number % -10)), end="")
- return(number % 10)
+    print("{:d}".format( - (number % -10)), end='')
+    return(number % 10)
