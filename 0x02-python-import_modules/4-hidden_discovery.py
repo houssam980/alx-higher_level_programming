3d492dbceb5bbb577587527915520374611b102d
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-02 11:22:41 -0700

    who

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    temp = dir(hidden_4)
+    for i in range(len(temp)):
+        for a in range(len(temp[i])):
+            if (temp[i][a] == '_' and temp[i][a+1] == '_'):
+                break
+            else:
+                print(temp[i])
