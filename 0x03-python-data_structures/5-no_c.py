320eab6b0b37d08d05ac3f70c9066202877e2835
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-03 23:53:50 -0700

    rm *Cc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    nw_str = my_string.translate({ord(ite): None for ite in "c", "C"})
+    nw_str = my_string.translate({ord(ite): None for ite in "cC"})
     return nw_str
