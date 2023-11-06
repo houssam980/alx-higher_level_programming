7c58dfe7070f5519e33f0dd79f8cec9080292793
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-06 08:56:07 -0800

    multi

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0 :
-        return sentence[0] == 0 ,"None"
+    tpl = ()
+    if len(sentence) == 0:
+        return sentence[0] == None
     else:
-        return len(sentence) ,sentence[0]
+        tpl = len(sentence), sentence[0]
+    return tpl
