315f6d31d3ddc181949d9e24ea15eda4c918259d
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-06 09:44:45 -0800

    div

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,7 @@ def divisible_by_2(my_list=[]):
     nw_nlist = []
     for iter in range(len(my_list)):
         if my_list[iter] % 2 == 0:
-            return nw_nlist , True
+             nw_nlist.append(True)
         else:
-            return nw_nlist, False
+            nw_nlist.append(False)
+    return nw_nlist
