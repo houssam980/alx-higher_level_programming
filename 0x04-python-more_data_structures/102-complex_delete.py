a41276cbfb10bdb3db43825502b5dc395f48ed12
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-08 12:07:29 -0800

    cmplex del

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    ls_k = list(a_dictionary.keys())
-    for value in ls_k:
-        if not value in a_dictionary:
-            return a_dictionary
-        else:
-            value == a_dictionary.get(value)
-            del a_dictionary[value]
+    ls_key = list(a_dictionary.keys())
+    for va in ls_key:
+        if value == a_dictionary.get(va):
+            del a_dictionary[va]
             return (a_dictionary)
