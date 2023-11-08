81dc76e04cabb4ec692f5da9c9123d410acb1378
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-08 11:36:09 -0800

    biggest

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    nw_score = 0
-    for s in a_dictionary:
-        if not s in a_dictionary:
-            return None
-        else:
-            nw_score = max(a_dictionary, isinstance(a_dictionary[s]))
-            return nw_score
+    if a_dictionary is None or a_dictionary == {}:
+        return None
+    big_score = max(a_dictionary.values())
+    for key, va in a_dictionary.items():
+        if va == big_score:
+            return key
