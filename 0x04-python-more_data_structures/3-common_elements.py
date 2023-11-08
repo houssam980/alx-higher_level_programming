0fbfcf27243b211c34cd8277ca797068ce3cca5a
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-08 07:05:53 -0800

    common with intersection

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for elem in set_1 and set_2:
-        if set_1[elem] == set_2[elem]:
-            return elem
+    return(set_1.intersection(set_2))
