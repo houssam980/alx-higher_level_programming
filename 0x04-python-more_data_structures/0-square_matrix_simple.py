6ceeda90287de948c5742dd846c9226521cbc57d
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-08 06:09:46 -0800

    first meet with lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,6 @@
 def square_matrix_simple(matrix=[]):
     nw_mtrx = []
     for iter in matrix:
- #using lambda = anonymous func
         rest = list(map(lambda x: x**2, iter))
         nw_mtrx.append(rest)
-    return nw_mtrx
+        return nw_mtrx
