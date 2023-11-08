656dc5147ef4aebe9a951f909def8c5fec1d78cf
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-08 06:24:57 -0800

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,8 @@
 def search_replace(my_list, search, replace):
     nw_list = []
     for iter in range (len(my_list)):
-        my_list[search] = replace
-        my_list = nw_list
-	return nw_list
+        if iter == search:
+            nw_list.append(replace)
+        else:
+            nw_list.append(iter)
+            return nw_list
