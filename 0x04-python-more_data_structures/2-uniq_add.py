f0002f9b67409e5030a1811ebe1938d87a84a59f
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-08 06:49:18 -0800

    add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    s = 0
+    nw_list = []
     for n in my_list:
-        s += n
+        if n not in nw_list:
+            s += n
+            nw_list.append(n)
         return s
