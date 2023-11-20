befaea90a39d54b08f9eef7707665f4286b9cd7a
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-20 05:58:05 -0800

    first int

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    idx = 0
+    try:
+        while True:
+            if isinstance(idx, int) and idx < x:
+                print("{:d}".format(my_list[idx]))
+                return idx
+            else:
+                print()
+    except IndexError:
+        print("list index out of range")
