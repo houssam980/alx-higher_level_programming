143a3b472d17b31aabfad52af601dfcb02994f26
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-20 06:51:50 -0800

    skkiped

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
 		ints += 1
             else:
                 print()
+	        return ints
         except (ValueError, TypeError):
-            continue
+            idx += 1
