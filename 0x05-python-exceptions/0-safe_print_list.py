2352576bc4c5cd4bd9a6bef6bfd0850b8c2f1031
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-20 05:24:16 -0800

    first try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/pytho3
 def safe_print_list(my_list=[], x=0):
     idx = 0
-    while my_list:
+    while True:
         try:
             if idx < x:
                 print(my_list[idx], end="")
-                idx += 1
+                break
             else:
                 print()
                 return idx
