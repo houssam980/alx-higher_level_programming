364a32bcf82e5cc5a5f4cbba71d828f328a0ee62
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-21 08:31:36 -0800

    first __prv

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Square Module based on 0-square.py task"""
+
+
+class Square:
+    """class That Defines a Square"""
+    def __init__(self, size):
+        """method with defult param self
+       and size
+       """
+        """ private instance __size"""
+        self.__size = size
