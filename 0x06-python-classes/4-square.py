e4229b703a0f999b7891ddea61b8bea2d2b3aee6
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-21 09:58:17 -0800

    props

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,20 +12,20 @@ class Square:
            TypeError : if size != 0
            ValueError : if size less than 0
         """
-	   self.size = size
-        
-	@property
-        def size(self):
-            """getting and setting"""
-            return (self.__size)
+        self.size = size
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-           elif size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @property
+    def size(self):
+            """getting and setting"""
+            return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """return the exact value of area by:
