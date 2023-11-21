f26ac47b1a55b83424ad99c11eff4d5601be449a
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-21 09:31:56 -0800

    value of area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,7 +12,7 @@ class Square:
            TypeError : if size != 0
            ValueError : if size less than 0
         """
-        if size != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
