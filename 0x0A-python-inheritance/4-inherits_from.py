e47c4e3d56ad964a1a35f1e2207786f24fdd7884
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-04 06:01:45 -0800

    is inhertens

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""define checking if onject instance of 
+"""define checking if onject instance of
    a class that inherited
  """
 
 
 def inherits_from(obj, a_class):
-   """
-   params: 
+    """
+    params :
     obj : object
     a_class : class
-    Return: true if onject instance of 
-   a class otherwise False
-   """
-   if isinstance(obj, a_class) and type(obj) != a_class:
-      return True
-   else:
-      return False
+    """
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
