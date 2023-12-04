b17cd4c470168859408fd520656b077de44b071d
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-04 06:11:55 -0800

    rasising

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define empty class BaseGeometry """
+
+
+class BaseGeometry:
+    def __init__(self):
+        """raising exception"""
+        def area(self):
+            raise Exception("area() is not implemented")
