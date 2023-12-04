a29aff5721740ed8b28939c92650e7484d250848
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-04 08:46:46 -0800

    equal not equal

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,7 +7,7 @@ class MyInt(int):
     def __eq__(self, n):
         """operators inverted  = != !="""
         return super().__ne__(n)
+
     def __ne__(self, n):
         """operators inverted  != == ="""
         return super().__eq__(n)
-
