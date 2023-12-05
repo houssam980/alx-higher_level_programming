bfbb5dd5a25801ab6ca8edefbf1cd6854c07e613
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-05 05:27:17 -0800

    convert to python stru

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,7 +4,7 @@ import json
 
 
 def from_json_string(my_str):
-    """Python data structure) represented 
+    """Python data structure) represented
        by a JSON string
     """
     return json.loads(my_str)
