948af1977234ed4af43a437205d4c2bda94f24c6
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-05 06:51:35 -0800

    first try

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""define Student Class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """adding new studient
+        params:
+        first_name = student first name
+        last_name = student last name
+        age = student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            for attrub in attrs:
+                return attrub == getattr(self, attrs)
+            else:
+                return self.__dict__
