33c65db7fcd05e00f86e16800e6344ff6d76a0f9
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-05 07:28:04 -0800

    insert

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,10 +11,10 @@ def append_after(filename="", search_string="", new_string=""):
         new_string: insert string
     """
     conte = ""
-    with open(filename) as f:
-        for ln in f:
+    with open(filename) as rd:
+        for ln in rd:
             conte += ln
             if search_string == ln:
                 conte += new_string
-    with open(filename, "w") as f:
-        f.write(conte)
+    with open(filename, "w") as wrt:
+        wrt.write(conte)
