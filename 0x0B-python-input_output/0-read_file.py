a53eb950c4c5a1b947d383148fe34675e7f19322
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-05 04:50:38 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,11 +5,4 @@
 def read_file(filename=""):
     with open("filename", "r" ,encoding="UTF8") as f:
         """print file's stdout"""
-        print(f.read(), end="")#!/usr/bin/python3
-	"""openining File using with"""
-
-
-	def read_file(filename=""):
-	    with open("filename", "r" ,encoding="UTF8") as f:
-	            """print file's stdout"""
-		            print(f.read(), end="")
+        print(f.read(), end=")
