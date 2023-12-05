42531b134604b6ded4ea460a23c4e08c03cccf61
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-05 04:48:08 -0800

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""openining File using with"""
+
+
+def read_file(filename=""):
+    with open("filename", "r" ,encoding="UTF8") as f:
+        """print file's stdout"""
+        print(f.read(), end="")#!/usr/bin/python3
+	"""openining File using with"""
+
+
+	def read_file(filename=""):
+	    with open("filename", "r" ,encoding="UTF8") as f:
+	            """print file's stdout"""
+		            print(f.read(), end="")
