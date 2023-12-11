f650342a5dcb1975a0f7783fe106f6a1a7fc2d42
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-11 11:42:21 -0800

    i really need

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""Defining Base class """
-from json import dumps
+"""Defining Base class"""
 
 
 class Base:
     __nb_objects = 0
-
     def __init__(self, id=None):
-        """costractor __init__"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
-    def to_json_string(list_dictionaries):
-        """to json str"""
-        if list_dictionaries == None or not list_dictionaries:
-            return "[]"
-        else:
-            return dumps(list_dictionaries)
+            id = Base.__nb_objects
 
