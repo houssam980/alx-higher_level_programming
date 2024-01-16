40a100664a52d320de89c7429f585af44cee10b1
houssam980 <houssam.ghmari1243@gmail.com>
2024-01-16 06:25:28 -0800

    create it

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---  create database without failing even it does exist
-CREATE DATABASE IF NOT EXISTS "hbtn_0c_0";
+-- create database without failing even it does exist.
+CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
