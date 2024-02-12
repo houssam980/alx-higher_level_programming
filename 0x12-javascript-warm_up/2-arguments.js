7b9d4fbcf9fd3a8fa17fd30d616693939aa3b2d6
houssam980 <houssam.ghmari1243@gmail.com>
2024-02-12 08:59:41 -0800

    args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.lenght <= 0){
+console.log("No argument")
+}
+else if (process.argv.lenght === 1){
+console.log("Argument found")
+}
+else {
+console.log("Arguments found")
+}
