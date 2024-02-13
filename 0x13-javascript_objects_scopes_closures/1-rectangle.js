e8a252d1166b0d69a9c772475e2800747503abd3
houssam980 <houssam.ghmari1243@gmail.com>
2024-02-13 04:27:53 -0800

    adding constractor

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h){
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 }
-module.exports = Rectangle
+
+
+module.exports = Rectangle;
