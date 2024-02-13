bccae29384440e6b7b72e75d1979c6a0909862f6
houssam980 <houssam.ghmari1243@gmail.com>
2024-02-13 05:06:17 -0800

    add print

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (var iter = 0; iter < this.height; iter++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
