740de9010b798971de607f0985348bafa91084d1
houssam980 <houssam.ghmari1243@gmail.com>
2024-02-15 05:30:35 -0800

    connection and list states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" script to list qll states """
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    
+    """making database connection"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+ 
+
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states")
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+        """close excution and db connection"""
+    curs.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+hey I'm readme file
