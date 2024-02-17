6ef7bb70c15aff0d1f0764cb8c20cacdfbbb8560
houssam980 <houssam.ghmari1243@gmail.com>
2024-02-17 13:51:22 -0800

    tired

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -3,27 +3,24 @@
  statment object
 """
 
-
 import sys
 from sqlalchemy import create_engine
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == '__main__':
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                            sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    Sess = sessionmaker(bind=eng)
-    Base.metadata.create_all(eng)
-    sessio = Sess()
+    sess = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    sessio = sess()
+    nw_state = State(name='Louisiana')
+    sessio.add(nw_state)
+    sessio.commit()
+    state_add = sessio.query(State).filter(State.name == 'Louisiana').one()
+    print(state_add.id)
 
-nw_state = stts(name ="Louisiana")
-sessio.add(nw_state)
-sessio.commit()
-
-stts_add = sessio.query(State).filter(State.name == "Louisiana").one()
-print(stts_add.id)
-
-sessio.close
-
+    sessio.close()
