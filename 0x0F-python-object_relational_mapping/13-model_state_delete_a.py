3c2dbbe59b944e3aa73e15f4e111fdba2c745081
houssam980 <houssam.ghmari1243@gmail.com>
2024-02-17 14:16:58 -0800

    soo tired

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+changes the name of a State object
+"""
+
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+
+    sess = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    sessio = sess()
+    states = sessio.query(State).filter(State.name.ilike('%a%')).all()
+    for st in states:
+        sessio.delete(st)
+    sessio.close()
