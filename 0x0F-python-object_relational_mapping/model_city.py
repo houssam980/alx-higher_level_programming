1ca59094676f7c767a9f74c9ed02df08f22cafa8
houssam980 <houssam.ghmari1243@gmail.com>
2024-02-17 14:38:39 -0800

    last one

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+city module
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer
+from sqlalchemy import ForeignKey
+from sqlalchemy import Column
+from sqlalchemy import String
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """base class for all cities"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
