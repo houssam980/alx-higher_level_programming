9e4d63a64613d215196037c828792a04ac2ca558
houssam980 <houssam.ghmari1243@gmail.com>
2024-02-17 13:19:09 -0800

    ists all State objects that contain the letter a from the database

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 """
-prints the first State object from the database
+  list all states with letter a 
 """
-from pip._internal.network import session
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+
+
 import sys
 from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
+if __name__ == '__main__':
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-if __name__ == "__main__":
-    eng = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    sess = sessionmaker(bind=eng)
+    Sess = sessionmaker(bind=eng)
     Base.metadata.create_all(eng)
-    sessio = sess()
+    sessio = Sess()
+    states = sessio.query(State).filter(State.name.ilike('%a%')) \
+                    .order_by(State.id).all()
 
-sts = session.query(State).filter(State.name.ilike('%a%'))\
-    .order_by(State.id).all()
-for state in sts:
-    print("{}: {}".format(state.id, state.name))
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        """closing session"""
     sessio.close()
