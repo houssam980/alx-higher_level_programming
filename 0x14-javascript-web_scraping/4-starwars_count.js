49da535c1c6f6b28a93ea8ddc2a7b3f7d122f714
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-26 21:47:33 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,20 @@
-#!/usr/bin/node
-const request = require('request');
+t request = require('request');
 const url = process.argv[2];
+
 request(url, function (err, data, body) {
-if (err) {
-console.log(err);
-} 
-else {
-let ctr = 0;
-const mvs = JSON.parse(body).results;
-for (let result = 0; result < mvs.length; result++) {
-const characters = mvs[result].characters;
-for (let j = 0; j < characters.length; j++) {
-if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-ctr += 1;
-}
-}
-}
-console.log(ctr);
-}
+  if (err) {
+    console.log(err);
+  } else {
+    let ctr = 0;
+    const mvs = JSON.parse(body).results;
+    for (let result = 0; result < mvs.length; result++) {
+      const characters = mvs[result].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
+          ctr += 1;
+        }
+      }
+    }
+    console.log(ctr);
+  }
 });
