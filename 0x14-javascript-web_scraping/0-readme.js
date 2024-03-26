4256744f60d5504a30cba489149652c5d70c8bc6
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-26 21:26:37 +0000

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const f_path = process.argv[2];
+const filePath = process.argv[2];
 const fs = require('fs');
-fs.readFile(f_path, 'utf8', function (err, data) {
-if (err) {
-console.log(err);
-} 
-else {
-console.log(data);
+fs.readFile(filePath, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
   }
 });
