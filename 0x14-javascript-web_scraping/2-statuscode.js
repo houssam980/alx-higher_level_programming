5c163ceb473522082dad09d359100422736fc87b
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-26 21:33:18 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
